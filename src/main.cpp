263d302302d78f79cc4eb1f8590eb4cbdd34879f
killercho <ggeorgi60@gmail.com>
2022-06-04 18:15:11 +0300

    Added basic Vector container.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "../headers/String.h"
+#include "../headers/Vector.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Make is working.";
+    Vector<int> intVector;
+    intVector.push(4);
+    intVector.push(2);
+    intVector.pop();
+    intVector.push(3);
+    intVector.push(10);
+    intVector.push(27);
+    intVector.pop();
+
+    std::cout << intVector.getSize() << '\n'
+              << intVector[1] << '\n';
+    std::cout << "Success!";
 }
